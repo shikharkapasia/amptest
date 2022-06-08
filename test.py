6822487c6a586242f15e5d586bb2659f2a2d72b2
Shikhar <shikharkapasia@gmail.com>
2022-06-08 12:57:21 +0530

    first

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ headers = {
     'X-Goog-Api-Key': 'AIzaSyC3V-YHo-ESvupSE-Ue1hdG_1gTX_rfFzQ',
 }
 
-data = '{urls: [\'https://example.com\']}'
+data = '{urls: "https://www.rankwatch.com/blog/competitor-monitoring/"."https://www.rankwatch.com/blog/"}'
 
 
 response = requests.post('https://acceleratedmobilepageurl.googleapis.com/v1/ampUrls:batchGet', headers=headers, data=data, verify=False)
