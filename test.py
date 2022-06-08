6195c816581dd50ba86525d51ae7f505d0a2f3d3
Shikhar <shikharkapasia@gmail.com>
2022-06-08 12:47:42 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+import argparse
+from cgitb import lookup
+import enum
+import requests
+import urllib3
+import certifi
+
+http = urllib3.PoolManager(
+    cert_reqs='CERT_REQUIRED',
+    ca_certs=certifi.where()
+)
+
+headers = {
+    'Content-Type': 'application/json',
+    'X-Goog-Api-Key': 'AIzaSyC3V-YHo-ESvupSE-Ue1hdG_1gTX_rfFzQ',
+}
+
+data = '{urls: [\'https://example.com\']}'
+
+
+response = requests.post('https://acceleratedmobilepageurl.googleapis.com/v1/ampUrls:batchGet', headers=headers, data=data, verify=False)
+
