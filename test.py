6f917a8911ce806060eae18956e78a7a14b801d8
Shikhar <shikharkapasia@gmail.com>
2022-06-10 11:27:05 +0530

    second

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ import urllib3
 import certifi
 import configparser
 
+parser = argparse.ArgumentParser()
+parser.add_argument("apikey", help="enter your api key")
+args = parser.parse_args()
+print(args.apikey)
+
 http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',
     ca_certs=certifi.where()
@@ -21,7 +26,7 @@ apikey = config['googleapi']['apikey']
 
 headers = {
     'Content-Type': 'application/json',
-    'X-Goog-Api-Key': apikey,
+    'X-Goog-Api-Key': args.apikey ,
 }
 
 data = '{urls: "https://www.rankwatch.com/blog/competitor-monitoring/"."https://www.rankwatch.com/blog/"}'
